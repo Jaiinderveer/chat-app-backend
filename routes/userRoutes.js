9847cc237cf902b8cfce65369a3a70f230e48941
jaiinderveer <jaiinderveersingh@gmail.com>
2026-03-27 21:45:54 +0530

    Day 2: Added route structure with userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,12 @@ const router = express.Router();
 router.get('/test', (req, res) => {
     res.send('User route working');
 });
+router.get('/login', (req, res) => {
+    res.send('Login route');
+});
+
+router.get('/register', (req, res) => {
+    res.send('Register route');
+});
 
 module.exports = router;
