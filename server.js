bec7226a5aed54d47fb0cea4176ec8ce5c001e10
jaiinderveer <jaiinderveersingh@gmail.com>
2026-03-27 21:36:17 +0530

    Day 2: Added route structure with userRoutes

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require('express');
-
 const app = express();
 
-// middleware
 app.use(express.json());
 
-// route
+// import routes
+const userRoutes = require('./routes/userRoutes');
+
+// use routes
+app.use('/api/users', userRoutes);
+
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
 
-app.get('/hello', (req, res) => {
-    res.send('Hello from backend');
-});
-// start server
 app.listen(5000, () => {
     console.log('Server running on port 5000');
 });
